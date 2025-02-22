d1aa019de1f1feb4e5ab1d53e331b2fe132a7d67
Allen Wang <--reset-author>
2025-02-22 15:34:25 -0600

    fixed some broken stuff

--- deeprl/cartpole_reinforce.py
+++ deeprl/cartpole_reinforce.py
@@ -9,19 +9,19 @@ import torch
 from torch import nn
 from torch.distributions.categorical import Categorical
 
-
-logger = logging.getLogger(__file__)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class Policy(nn.Module):
-    def __init__(self, state_shape, action_shape):
+    def __init__(self, state_shape, action_shape, hidden_size=128):
         super().__init__()
         self.state_shape = state_shape
         self.action_shape = action_shape
         self.model = nn.Sequential(
-            nn.Linear(np.prod(state_shape), 128), nn.ReLU(inplace=True),
-            nn.Linear(128, 128), nn.ReLU(inplace=True),
-            nn.Linear(128, np.prod(action_shape)),
+            nn.Linear(np.prod(state_shape), hidden_size), nn.ReLU(inplace=True),
+            nn.Linear(hidden_size, hidden_size), nn.ReLU(inplace=True),
+            nn.Linear(hidden_size, np.prod(action_shape)),
         )
 
     def forward(self, obs):
@@ -60,8 +60,7 @@ def run_policy(policy, visualize=False):
 
     episode_over = False
     while not episode_over:
-        action = policy.get_action(obs)
-        action = env.action_space.sample()  # agent policy that uses the observation and info
+        action = policy.get_action(obs).item()
         next_obs, reward, terminated, truncated, info = env.step(action)
         episode_over = terminated or truncated
         total_reward += reward
@@ -79,15 +78,14 @@ def evaluate(num_eval_episodes, policy):
     return np.mean(episodic_rewards)
 
 
-
 def main():
     config = dict(
         use_wandb=True,
         env="CartPole-v1",
         seed=42,
-        n_episodes=100,
-        step_size=0.01,
-        discount=0.9,
+        hidden_size=256,
+        n_episodes=1000,
+        discount=0.99,
         learning_rate=2.5e-4,
         num_eval_episodes=10,
     )
@@ -96,6 +94,7 @@ def main():
         mode="online" if config["use_wandb"] else "disabled",
         project=f"{config['env']}-reinforce",
         config=config,
+        resume="allow",
     )
     torch.manual_seed(config["seed"])
     logger.info(f"creating training env {config['env']}")
@@ -104,19 +103,20 @@ def main():
     action_shape = train_env.action_space.n
     state_shape = train_env.observation_space.shape or train_env.observation_space.n
     logger.info(f"Creating policy with action_shape {action_shape} and state_shape {state_shape}")
-    policy = Policy(action_shape=action_shape, state_shape=state_shape)
+    policy = Policy(action_shape=action_shape, state_shape=state_shape, hidden_size=config["hidden_size"])
     logger.info(f"Creating Adam optimizer with learning rate {config['learning_rate']}")
     optim = torch.optim.Adam(policy.parameters(), lr=config["learning_rate"], eps=1e-5)
 
-    global_step = 0
     
     for episode_num in range(config["n_episodes"]):
         total_return = 0
-        trajectory_len = 0
         obs, info = train_env.reset()
         episode_over = False
-        episodic_return = 0  # Initialize return at start of episode
+
+        rewards = []
+        log_probs = []
         
+        # generate trajectory
         while not episode_over:
             # Get action and its log probability
             logits = policy(obs)
@@ -127,41 +127,39 @@ def main():
             # Take action in environment
             next_obs, reward, term, trunc, info = train_env.step(action.item())
             episode_over = term or trunc
+            rewards.append(reward)
+            log_probs.append(log_prob)
+            obs = next_obs  # Update observation
 
+        episodic_return = 0
+        returns = []
+        for reward in reversed(rewards):
             # Calculate return (G)
             episodic_return = reward + config["discount"] * episodic_return
             total_return += reward
-
-            # Calculate loss and update policy
-            loss = -log_prob * episodic_return
-            optim.zero_grad()
-            loss.backward()
-            optim.step()
-
-            obs = next_obs  # Update observation
-
-            wandb.log(dict(
-                episodic_return=episodic_return,
-                grad_norm=calculate_grad_norm(policy),
-            ), step=global_step)
-
-            trajectory_len += 1
-            global_step += 1
-
+            returns.append(episodic_return)
+        returns = torch.tensor(list(reversed(returns)))
+        # Calculate loss and update policy
+        policy_loss = []
+        for log_prob, episodic_return in zip(log_probs, returns):
+            policy_loss.append(-log_prob * episodic_return)
+        loss = torch.stack(policy_loss).sum()
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
         eval_returns = evaluate(config["num_eval_episodes"], policy)
-
         episodic_metrics = dict(
-            trajectory_len=trajectory_len,
-            episodic_return=episodic_return,
+            trajectory_len=len(returns),
             total_return=total_return,
+            grad_norm=calculate_grad_norm(policy),
             eval_returns=eval_returns)
-
         logger.info(f"[ep_step={episode_num}] {episodic_metrics}")
         wandb.log(episodic_metrics, step=episode_num)
-
-    run_policy(policy, visualize=True)
+    
+    logging.info("Evaluating the final policy...")
+    ret = run_policy(policy, visualize=True)
+    logging.info(f"Return: {ret}")
 
 
 if __name__ == "__main__":
-    logger.setLevel(logging.INFO)
     main()
